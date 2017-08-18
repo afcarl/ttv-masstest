14c5cc79a8d498b2129c99a87e208cdd16ac37e2
Rejean Leblanc <rejleb.uni@gmail.com>
2017-08-18 11:39:52 -0400

    state class working probably

--- ttvfaststate.py
+++ ttvfaststate.py
@@ -6,9 +6,10 @@ import ttvfast
 import scipy.stats
 import copy
 
-class State(object):
+class TTVState(object):
     def __init__(self, stel_m_planets, ttvfast_settings):
         self.stel_m_planets = stel_m_planets
+        self.ttvfast_settings = ttvfast_settings
         self.Time = ttvfast_settings[0]
         self.dt = ttvfast_settings[1]
         self.Total = ttvfast_settings[2]
@@ -26,14 +27,15 @@ class State(object):
             self.logp = -self.get_chi2(obs)
         return self.logp + softlnpri
         
-    def get_chi2(obs):
+    def get_chi2(self, obs):
         #split and prep data for ttvfast
-        for i in range(self.n_plan):
-            
-        planets = [:]
-        stellar__mass = self.stel_m_planets[0]
+        params = self.stel_m_planets
+        planet1 = ttvfast.models.Planet(*params[1:1 + 7])
+        planet2 = ttvfast.models.Planet(*params[1 + 7:])
+        stellar_mass = params[0]
+        planets = [planet1, planet2]            
         #get results...
-        results = ttvfast.ttvfast(planets, stellar_mass, Time, dt, Totalx`)
+        results = ttvfast.ttvfast(planets, stellar_mass, self.Time, self.dt, self.Total)
         #prep results
         integer_indices, epochs, times, rsky, vsky = results["positions"]
         ttimes = times[:375]
@@ -47,10 +49,10 @@ class State(object):
                 t2_times.append(ttimes[i])
         #calc chi2
         chi2 = 0.
-        for i in range(t1):
-            chi2 += 
+        fac = len(t1_times)
+        for i in range(len(t1_times)):
+            chi2 += (t1_times[i] - obs.times[i])**2. * 1./(obs.errors[i])**2. * fac
         return chi2
-
 
     def get_params(self):
         return self.stel_m_planets
@@ -60,11 +62,12 @@ class State(object):
     
     #Will need to modify this...
     def deepcopy(self):
-        return State(copy.deepcopy(self.obs)
+        return TTVState(copy.deepcopy(self.stel_m_planets), copy.deepcopy(self.ttvfast_settings))
 
     def priorHard(self):
         for i in enumerate(self.stel_m_planets):
-            if (self.params[2] <= 0.00001) :
+            if (self.stel_m_planets[2] <= 0.00001) :
                 print "Invalid state was proposed (m1)"
                 return True
         return False
+
